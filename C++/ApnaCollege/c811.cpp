791901dfc5c3c66f93d7119567902c166a09dba2
mihirsinghyadav <mihirsinghyadav@gmail.com>
2020-12-20 20:49:19 +0530

    Arrays

---
+++ C++/ApnaCollege/c811.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int array[4] = {10, 20, 30, 40};
+    cout << array[3] << endl;
+    return 0;
+}
---
+++ C++/ApnaCollege/c842.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+// Sum of all subarrays
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int curr = 0;
+    for (int i = 0; i < n; i++)
+    {
+        curr = 0;
+        for (int j = i; j < n; j++)
+        {
+            curr += a[j];
+            cout << curr << endl;
+        }
+    }
+    return 0;
+}
